178ef995872a4c291350176d033d1299608c2f13
Lucas Belinelli Pereira <lucas.pereira@ykt.com.br>
2021-03-23 21:46:26 -0300

    comit final

--- cypress/integration/primeiro_test.spec.js
+++ cypress/integration/primeiro_test.spec.js
@@ -9,15 +9,22 @@ describe('Calculadora de imposto', () => {
 
         cy.get('[name=nome]').should('have.text', '');
         cy.get('[name=idade]').should('have.text', '');
-        cy.get('[name=sexo]').should('have.value', '-');
+        cy.get('[name=sexo]').should('have.value', 'M');
         cy.get('[name=salario]').should('have.text', '');
     });
 
     it('deve calcular o imposto devido', () => {
-        // preencha todos os campos
-
-        // clique no botao calcular
-
-        // verifique (usando .should) a saída retornada com o calculo do imposto devido
+        cy.visit('https://andreendo.github.io/sample-ui-compras/index-2.html');
+        cy.get('[name=nome]').type('Lucas');
+        cy.get('[name=idade]').type('21');
+        cy.get('[name=sexo]').select('M');
+        cy.get('[name=salario]').type('2000');
+         
+        cy.get(`[value='Calcular']`).click();
+         
+        cy.get('[id=divValorImposto]').should('have.text', 'Valor a pagar de imposto: 300');
+        cy.get('[id=divNome]').should('have.text', 'Nome: Lucas');
+        cy.get('[id=divIdade]').should('have.text', 'Idade: 21');
+        cy.get('[id=divSexo]').should('have.text','Sexo: M');
     });
 });
